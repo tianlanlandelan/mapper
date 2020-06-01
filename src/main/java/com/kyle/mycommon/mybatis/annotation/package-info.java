443682035ab7ea40389c4ef4cdfd1811a52d004b
yangkaile <guyexing@foxmail.com>
2020-06-01 08:45:36 +0800

    初始化

--- src/main/java/com/kyle/mycommon/mybatis/annotation/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 自定义注解，用于标注表名、字段名、索引等数据库映射关系
- */
-package com.kyle.mycommon.mybatis.annotation;
--- src/main/java/com/kyle/mycommon/mybatis/provider/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * 提供基础的 Mybatis SQL语句
- */
-package com.kyle.mycommon.mybatis.provider;
